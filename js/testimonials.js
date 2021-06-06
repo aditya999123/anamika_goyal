1c1cb5936c35db6d99787f95856fd9f48a1caf66
Naveen Sundar <naveen.s@fyle.in>
2021-06-06 20:12:35 +0530

    carousal themed

--- js/testimonials.js
+++ js/testimonials.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
-    $('.your-class').slick({
+    $('.reviews-list').slick({
         speed: 300,
         slidesToShow: 3,
         slidesToScroll: 1,
         infinite: true,
         dots: true,
+        appendDots: '#testimonials .review-dots',
+        prevArrow: '<div class="btn-holder"><button type="button" class="btn slick-next"><i class="fas fa-chevron-left"></i></button></div>',
+        nextArrow: '<div class="btn-holder"><button type="button" class="btn slick-next"><i class="fas fa-chevron-right"></i></button></div>',
         responsive: [{
                 breakpoint: 1024,
                 settings: {
@@ -34,4 +37,5 @@ $(document).ready(function () {
             }
         ]
     });
+
 });
